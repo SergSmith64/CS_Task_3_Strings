fd238ddfb09fba73020bdeb913b914af57f8b4e8
SergSmith64 <sergsmith2008@yandex.ru>
2019-04-27 19:33:04 +0400

    commit_02_Task_Completed

---
+++ Task_3_Strings/Task_3_Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВЕДИТЕ СТРОКУ");
+            string stroka = Console.ReadLine();
+            Console.WriteLine("Вы ввели - " + stroka);
+
+            Console.WriteLine("ВВЕДИТЕ подстроку");
+            string podstroka = Console.ReadLine();
+            Console.WriteLine("Вы ввели - " + podstroka);
+
+            // ПРОВЕРКА ДЛИНЫ
+            if (stroka.Length < podstroka.Length)
+                Console.WriteLine("строка на может быть меньше подстроки");
+
+            else
+            {
+                if (stroka.Contains(podstroka))
+                    Console.WriteLine("Строка {0} содержит подстроку {1}", stroka, podstroka);
+                else
+                    Console.WriteLine("Строка {0} НЕ содержит подстроку {1}", stroka, podstroka);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
